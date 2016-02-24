3c93a19f9b1bbd0633c5ee6a053e499eafd29e5a
Ty Cobb <ty.cobb.m@gmail.com>
2016-02-24 13:01:31 -0600

    Initial Commit

---
+++ Drip/Drip.h
@@ -0,0 +1,19 @@
+//
+//  Drip.h
+//  Drip
+//
+//  Created by Ty Cobb on 2/24/16.
+//  Copyright © 2016 wizrad. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Drip.
+FOUNDATION_EXPORT double DripVersionNumber;
+
+//! Project version string for Drip.
+FOUNDATION_EXPORT const unsigned char DripVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Drip/PublicHeader.h>
+
+
